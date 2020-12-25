eeebd8c62fc0d0798243eb8cb016148e3ea48094
kocierik <kocierik@gmail.com>
2020-12-25 14:13:42 +0100

    feat: add move a pointer

--- main.cpp
+++ main.cpp
@@ -4,18 +4,39 @@
 #include <curses.h>
 
 using namespace std;
-
 int main(){
+  int c,x=0,y=0;
   initscr();
+  keypad(stdscr,TRUE);
   noecho();
-  raw();
-
-  int c, x, y;
-  getmaxyx(stdscr,y,x);
-  move(y/2,x/2);
-  printw("§");
-  getch();
-  refresh();  
-  endwin();
-  return 0;
+  c=getch();
+  while(c!=27)
+  {
+    switch (c)
+    {
+    case KEY_RIGHT:
+      x++;
+      move(y,x);
+      break;
+    case KEY_LEFT:
+      x--;
+      move(y,x);
+      break;
+    case KEY_UP:
+      y--;
+      move(y,x);
+      break;
+    case KEY_DOWN:
+      y++;
+      move(y,x);
+      break;
+    default:
+      printw("%c",c);
+      x++;
+      break;
+    }
+  c=getch();
+}
+endwin();
+return 0;
 }
